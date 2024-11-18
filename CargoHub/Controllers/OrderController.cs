902c1b31a9e7e91856ab6b094fac1ea49ccbdbd7
SvenvanMuiden <Svenvm2004@gmail.com>
2024-11-18 15:29:51 +0100

    update orders kunnen nu gemaakt worden met null als shipmentid

--- CargoHub/Controllers/OrderController.cs
+++ CargoHub/Controllers/OrderController.cs
@@ -63,7 +63,7 @@ namespace CargoHub.Controllers
                     WarehouseId = orderDto.WarehouseId,
                     ShipTo = orderDto.ShipTo ?? 0,
                     BillTo = orderDto.BillTo ?? 0,
-                    ShipmentId = orderDto.ShipmentId ?? 0,
+                    ShipmentId = orderDto.ShipmentId ?? null,
                     TotalAmount = orderDto.TotalAmount,
                     TotalDiscount = orderDto.TotalDiscount,
                     TotalTax = orderDto.TotalTax,
